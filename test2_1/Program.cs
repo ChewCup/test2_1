b70d97b600445eea27c8f3aea6ef7af5fc1bbfe7
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-10-13 17:43:29 +0200

    Add project files.

---
+++ test2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dia;
+
+            Console.Write("Ange cirkelns diameter: ");
+
+            dia = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("En cirkel med diameter " + dia + " har: ");
+            Console.WriteLine();
+            Console.Write("... omkretsen: " + dia * Math.PI);
+            Console.WriteLine();
+            Console.Write("... arean: " + Math.PI * Math.Pow(4, 2));
+         
+            //stop
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
